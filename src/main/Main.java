b6724a688d005a60c85597ecfdf0fee4c4228ffe
hanky86 <hanky0721@gmail.com>
2017-02-19 17:13:32 +0900

    My second git commit

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.printf("My First Git");
+		System.out.printf("My Second Git Commit");
+		
+		
+	}
+
+}
